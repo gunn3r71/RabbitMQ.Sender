7428a5daf0471c9cd9fe84cc07ab4b3d1a5cc9c2
= <=>
2021-07-12 17:56:09 -0300

    Configurando consumer

---
+++ Worker/Domain/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Domain
+{
+    public class Order
+    {
+        public int Number { get; set; }
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return "Product\n\n" +
+                   $"Number: {this.Number}" +
+                   $"Name: {this.ItemName}" +
+                   $"Price: {this.Price:F2}";
+        }
+    }
+}
---
+++ Worker/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Text.Json;
+using API.Domain;
+using RabbitMQ.Client.Events;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "orderQueue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var order = JsonSerializer.Deserialize<Order>(message);
+
+                    Console.WriteLine(" [x] {0}", order);
+                };
+                channel.BasicConsume(queue: "orderQueue",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
